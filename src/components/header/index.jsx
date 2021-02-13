7790f98eaef740dbb5f2e81f9c3bc00b2d4d4bf8
ashishdas009 <das.ashish50@gmail.com>
2021-02-13 15:18:08 -0500

    adding email

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -62,7 +62,7 @@ const Header = ({ metadata = {}, noBlog = false }) => {
           )}
 	  {email && (
 	    <li className={classes.item}>
-	      <a className={classes.link} href{email}>
+	      <a className={classes.link} href={email}>
 		Email
 	      </a>
 	    </li>
